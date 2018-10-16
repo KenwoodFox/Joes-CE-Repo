9b5107c5de51b17244d95b49864c692346301f1d
KenwoodFox <31870999+KenwoodFox@users.noreply.github.com>
2018-10-16 12:57:24 -0400

    Changed a lot..
    Should work

--- TumbleBot Basic/easyGas.h
+++ TumbleBot Basic/easyGas.h
@@ -1,14 +1,13 @@
 //this function by joe, remeber to include my drivetrain.c before including this file
 
-void easyGas(int targetSpeed, int accelValue, float accelDirection)
-{
-	float speed = 0; //arb value for the current speed
+void easyGas(int robotSpeed, int targetSpeed, int accelValue, float accelDirection)
+{ //The speed the robot is going, the speed you want it to go, the value of deacceleration, the direction and magnitude
 
-	while(speed != targetSpeed || speed > 127) //while you are not at target speed OR the speed value is bigger than the max output, THIS WILL TRIGGER IN CASE OF AN ERROR
+	while(robotSpeed != targetSpeed && robotSpeed < 127 && robotSpeed > -127) //while you are not at target speed OR the speed value is bigger than the max output, THIS WILL TRIGGER IN CASE OF AN ERROR
 	{
-		speed = speed + accelDirection; //add accelDirection to the speed value
+		robotSpeed = robotSpeed + accelDirection; //add accelDirection to the speed value
 		delay(accelValue); //delay for this long
-		starboardDriveTrain(speed); //set the speed
-		portDriveTrain(speed);
+		starboardDriveTrain(robotSpeed); //set the speed
+		portDriveTrain(robotSpeed);
 	}
 }
