e4d2e21c54cbd9bb5b49771394ca5dd3169f0f6e
KenwoodFox <31870999+KenwoodFox@users.noreply.github.com>
2018-10-18 12:39:45 -0400

    Changed values
    Changed some values for the code, looks like theres a Bug in the code somehwere around the slowflash for loop, ill look into it, might just remove it.

--- Joe/Tumbler Bot/TumbleBot Basic/drivetrain.h
+++ Joe/Tumbler Bot/TumbleBot Basic/drivetrain.h
@@ -10,7 +10,7 @@ void starboardDriveTrain(float speed) //function to drive each of the starboard 
 
 void portDriveTrain(float speed) //function to drive each of the port wheels using value speed,
 {
-	int tune = 1.0050; //tune adjust for the variablity of the wheels
+	int tune = 1.0000; //tune adjust for the variablity of the wheels
 
 	motor[portFore] = (speed * tune); //Set both motors to the speed given and multiply them by thr tune value
 	motor[portAft] = (speed * tune);
