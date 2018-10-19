f1bd96c5e5c42d265340ecadbcc53009c76e7f70
KenwoodFox <31870999+KenwoodFox@users.noreply.github.com>
2018-10-19 11:03:30 -0400

    Moved the init void outside
    Moved the init void outside the source file

--- Joe/Tumbler Bot/TumbleBot Basic/source.c
+++ Joe/Tumbler Bot/TumbleBot Basic/source.c
@@ -8,29 +8,7 @@
 
 #include "drivetrain.h" //include the drivetrain header
 #include "easyGas.h" //include the easygas header
-
-void startup()
-{
-	int i = 0;
-
-	//Startup notifier
-	while(SensorValue[bumpSwitch] != true) //while bumpswitch is not pressed
-	{
-		delay(50); //wait 50ms
-		SensorValue[LED] = true; //Blink LED
-		delay(50);
-		SensorValue[LED] = false;
-	}
-
-	while( i < 6) //runs after bump switch is pressed
-	{
-		delay(100); //wait 100ms
-		SensorValue[LED] = true; //slow Blink
-		delay(100);
-		SensorValue[LED] = false;
-		i++;
-	}
-}
+#include "init.h" //Include init.h, init.h also includes void startup();
 
 task main()
 {
