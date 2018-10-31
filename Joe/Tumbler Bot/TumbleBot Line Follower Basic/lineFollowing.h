0bbc01053206c0bc25fdbacc8e5ed6a6e05050b7
KenwoodFox <31870999+KenwoodFox@users.noreply.github.com>
2018-10-31 10:14:07 -0400

    Just changing this one thing to test the new API bot sorry :3
    I changed just some formating, really only to check with the API bot

--- Joe/Tumbler Bot/TumbleBot Line Follower Basic/lineFollowing.h
+++ Joe/Tumbler Bot/TumbleBot Line Follower Basic/lineFollowing.h
@@ -19,7 +19,6 @@ void linePID(int speed, char statusLight) //serious maths
 	int threshold = 400; //The value at witch we can differ a black line from a white table, we could make this a self calibrating value!!
 
 	float error = SensorValue[starboardReflector] - SensorValue[portReflector]; //rescan the error
-
 	/* In a PID loop, the goal is to aproach the target, but not overshoot it, a good PID loop can swoosh right to the target,
 	* while many others simply just, rush past it and come back right after, this mathamtical form of position control has been
 	* around since the first basic electronic navigation systems
